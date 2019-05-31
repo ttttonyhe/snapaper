41857ccb7b644bdec3e840ec5d3869dbf3ec0ea9
TonyHe <he@holptech.com>
2019-05-31 23:29:13 +0800

    Update Pages Style

--- donate.php
+++ donate.php
@@ -1,28 +1,15 @@
 <?php require 'header.php' ?>
-<div class="uk-container" style="margin-top: 6%;">
-    <div class="sub-title-div" style="margin-bottom:60px;display: flex;">
-        <div style="
-	width: 97%;
-">
-            <h1 class="sub-title-h1">Donate to Us</h1>
-            <p class="sub-title-p">If you are feeling comfortable...</p>
-        </div>
-        <div style="text-align: right;padding-top: 30px;">
-            <a onclick="history.go(-1);"><svg width="40" height="40" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" class="uk-icon uk-svg" style="margin-right: 15px;color: #333;margin-top: -1px;">
-                    <line fill="none" stroke="#000" x1="5.5" y1="9.5" x2="15" y2="9.5"></line>
-                    <line fill="none" stroke="#000" x1="5" y1="1.5" x2="9" y2="1.5"></line>
-                    <polyline fill="none" stroke="#000" stroke-width="1.03" points="8.5,6.5 5.5,9.5 8.5,12.5"></polyline>
-                    <polyline fill="none" stroke="#000" points="12.5,7 12.5,2.5 9.5,2.5 9.5,3.5 4.5,3.5 4.5,2.5 1.5,2.5 1.5,15.5 12.5,15.5 12.5,12"></polyline>
-                </svg></a>
-        </div>
-    </div>
 
-
-    <div class="uk-card uk-card-default uk-card-body" style="padding: 100px 25vh;">
+<div class="uk-container page-container">
+    <div class="uk-card uk-card-default uk-card-body page-body">
+    	<div class="page-top-div">
+        	<h1 class="page-title">Donate to Us</h1>
+        	<p class="page-des">If you are feeling comfortable...</p>
+        	</div>
         <p>If you feel that my project is helpful to you and you are willing to give me a little support, you can donate to me in the following ways, which will help me to maintain the continuous development of this project, thank you very much!
             <br>
         </p>
-        <table style="border-color: #eee;border-collapse: collapse;border: 1px solid #eee;line-height: 2;width: 100%;text-align: center;font-weight: 600;">
+        <table class="page-donate-table">
             <tbody>
                 <tr>
                     <td>
@@ -61,11 +48,20 @@
                     <td>¥40.00</td>
                     <td>07/04/2019</td>
                 </tr>
+                <tr>
+                    <td>Mr.Cowsill</td>
+                    <td>¥2.50</td>
+                    <td>29/05/2019</td>
+                </tr>
+                <tr>
+                    <td>Harold</td>
+                    <td>¥2.50</td>
+                    <td>29/05/2019</td>
+                </tr>
             </tbody>
         </table>
         <br>
     </div>
 </div>
-</body>
-
+	</body>
 </html>
